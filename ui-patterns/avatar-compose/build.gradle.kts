289d8fe76615ff7c659ab6cf736cb4333f7675bc
thilawfabrice <fabricethilaw@gmail.co>
2022-12-15 02:20:46 +0100

    Adding Avatar compose project

---
+++ ui-patterns/avatar-compose/build.gradle.kts
@@ -0,0 +1,30 @@
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+
+        if (!libs.versions.compose.snapshot.get().endsWith("SNAPSHOT")) {
+            maven { url = uri("https://androidx.dev/snapshots/builds/${libs.versions.compose.snapshot.get()}/artifacts/repository/") }
+        }
+    }
+    dependencies {
+        classpath(libs.android.gradlePlugin)
+        classpath(libs.kotlin.gradlePlugin)
+    }
+}
+
+subprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+// Required by the "version catalog update" plugin to help keep
+// the versions in the Gradle version catalog toml file up to date.
+plugins {
+    id("com.github.ben-manes.versions") version "0.43.0"
+    id("nl.littlerobots.version-catalog-update") version "0.7.0"
+}
+
+apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
